2e09dd31f4709a1b3cca29ac5d1b2a9b116bf958
nilcevc <123847308+nilcevc@users.noreply.github.com>
2025-02-23 19:08:19 -0600

    Primer commit

---
+++ src/main/java/org/cenfo/tareacrud/TareaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.cenfo.tareacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cenfo/tareacrud/TareaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cenfo.tareacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
